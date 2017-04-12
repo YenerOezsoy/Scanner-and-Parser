9b0864ab00a3207c9753e2899afc2e2d32c41c1a
yenerozsoy <yenerozsoy@192.168.0.103>
2017-04-12 17:25:46 +0200

    Scanner und Token Klasse implementiert ToDo: Testen, Testen, ...

--- Scanner/includes/Scanner.h
+++ Scanner/includes/Scanner.h
@@ -8,10 +8,30 @@
 #ifndef SCANNER_H_
 #define SCANNER_H_
 
+
+#include "Buffer.h"
+#include "Automat.h"
+//#include "Symboltabelle.h"
+#include "Token.h"
+
 class Scanner {
-public:
-	Scanner();
-	virtual ~Scanner();
+        int i = 0;
+        int row = 1;
+        int column = 1;
+        bool stop = false;
+
+        Automat *automat;
+        Buffer *buffer;
+        //Symboltabelle *symboltabelle;
+        Token *token;
+
+        int type = 7;
+        int previousType;
+
+        char c;
+
+public: Token* nextToken();
+        Scanner();
 };
 
 #endif /* SCANNER_H_ */
